fd5f8978735399769eee3a93cf24ce72a0bb122d
01LEE <tkqqkrrhf11@gmail.com>
2025-05-08 16:08:04 +0900

    ChatPDF v0.0.1

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def pdf_to_document(uploaded_file):
     temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
     with open(temp_filepath, "wb") as f:
         f.write(uploaded_file.getvalue()) 
-    loader=PyPDFLoader("unsu.pdf")
+    loader=PyPDFLoader(temp_filepath)
     pages = loader.load_and_split()
     return pages
 
